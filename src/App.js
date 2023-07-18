c01f4230ab2eebd7e77d1346423eca7675d059d8
Jan Golder <kontakt.jangolder@gmail.com>
2023-07-18 21:07:59 +0200

    add basic validation logic

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import UsersList from "./components/UsersList";
 
 function App() {
   const [usersData, setUsersData] = useState([]);
+  const [isValid, setIsValid] = useState(true);
 
   const usersDataHandler = (userData) => {
-    setUsersData((prevState)=>{
-      return [...prevState, userData]
-    })
+    if (userData.username.length < 3 || userData.age < 10) {
+      console.log("Username is too short or you are too young to subscribe");
+      setIsValid(false);
+      return;
+    } else {
+      setIsValid(true);
+      setUsersData((prevState) => {
+        return [...prevState, userData];
+      });
+    }
   };
-
 
   return (
     <>
       <MainForm onUsersDataHandler={usersDataHandler} />
-      <UsersList usersData={usersData}/>
+      <UsersList usersData={usersData} />
+      {/* MODAL */}
     </>
   );
 }
