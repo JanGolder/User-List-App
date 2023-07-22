b4094d2702b8cf19290eabcd62efeb9280bfee86
Jan Golder <kontakt.jangolder@gmail.com>
2023-07-22 17:02:13 +0200

    add Portals hook to validation component

--- src/App.js
+++ src/App.js
@@ -31,8 +31,6 @@ function App() {
 
   }
 
-  console.log(usersData)
-
   return (
     <>
       <MainForm onUsersDataHandler={usersDataHandler} />
--- src/components/ValidationInfo.js
+++ src/components/ValidationInfo.js
@@ -1,15 +1,23 @@
+import ReactDOM from 'react-dom';
 import classes from './ValidationInfo.module.css';
 
 const ValidationInfo = ({onCloseHandler})=>{
 
+
+const ValidationModal = ({onClick})=>{
     return (
-        <div onClick={onCloseHandler} className={classes['module-wrap']}>
-            <div className={classes.backdrop}></div>
-            <div className={classes.module}>
-                <p>Username is too short or you are too young to subscribe.</p>
-                <button onClick={onCloseHandler}>Close</button>
-            </div>
+        <div onClick={onClick} className={classes['module-wrap']}>
+        <div className={classes.backdrop}></div>
+        <div className={classes.module}>
+            <p>Username is too short or you are too young to subscribe.</p>
+            <button onClick={onClick}>Close</button>
         </div>
+    </div>
+    )
+}
+
+    return (
+        ReactDOM.createPortal(<ValidationModal  onClick={onCloseHandler} />, document.querySelector('#root-overlay'))
     )
 }
 
